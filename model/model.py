bd9c91d413cf19c89097903bc59c2d54edfcf8ac
Mohd Kaif <98801504+KaifAhmad1@users.noreply.github.com>
2024-08-08 18:00:50 +0530

    Update model.py

--- model/model.py
+++ model/model.py
@@ -2,17 +2,30 @@ from langchain.embeddings import HuggingFaceBgeEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.docstore.document import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import ChatGroq
+from config.apikeys import GROQ_API_KEY
 import logging
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+# Initialize LLM
+def initialize_llm():
+    llm = ChatGroq(
+        temperature=0,
+        model="mixtral-8x7b-32768",
+        api_key=GROQ_API_KEY
+    )
+    return llm
+
+# Text Chunking Function
 def chunk_text(text, chunk_size=500, chunk_overlap=50):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     chunks = text_splitter.split_text(text)
     return chunks
 
+# Process and Store Data in Vector DB
 def process_and_store_data(data):
     embeddings = HuggingFaceBgeEmbeddings(
         model_name="BAAI/bge-small-en",
@@ -38,4 +51,3 @@ def process_and_store_data(data):
     else:
         print("No valid documents to process. Creating an empty vector store.")
         return FAISS.from_texts(["No data available"], embeddings)
-
